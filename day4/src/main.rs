7e07973ca445723f52379a1a3daaa82006f390f2
FranseFrikandel <romain@ceracarbon-racing.com>
2024-12-04 16:59:43 +0100

    Day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,7 +1,5 @@
 use std::fs::read_to_string;
 
-const search_arr: [char; 4] = ['X', 'M', 'A', 'S'];
-
 fn main() {
     let mut file_vec: Vec<Vec<char>> = Vec::new();
     let mut matches: usize = 0;
@@ -9,9 +7,10 @@ fn main() {
         file_vec.push(line.trim().chars().collect());
     }
     
-    for j in 0..file_vec.len() {
-        for i in 0..file_vec[j].len() {
-            if file_vec[j][i] == search_arr[0] {
+    // We don't need to search the edges, since there can't be an X shape there.
+    for j in 1..file_vec.len()-1 {
+        for i in 1..file_vec[j].len()-1 {
+            if file_vec[j][i] == 'A' {
                 matches += search(i as isize, j as isize, &file_vec);
             }
         }
@@ -20,40 +19,15 @@ fn main() {
 }
 
 fn search(x: isize, y: isize, arr: &Vec<Vec<char>>) -> usize {
-    let dirs: [[isize;2]; 8] = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [0, -1], [1, -1], [1, 0], [1, 1]]; // [y, x]
+    let dirs: [[isize;2]; 4] = [[-1, -1], [-1, 1], [1, -1], [1, 1]]; // [y, x]
     let mut matches: usize = 0;
     for dir in dirs {
-        if search_dir(x, y, arr, dir[0], dir[1]) {
+        if arr[(y+dir[0]) as usize][(x+dir[1]) as usize] == 'M' && arr[(y-dir[0]) as usize][(x-dir[1]) as usize] == 'S' {
             matches += 1;
         }
     }
-    return matches;
-}
-
-fn search_dir(x: isize, y: isize, arr: &Vec<Vec<char>>, i: isize, j: isize) -> bool {
-    let mut cur_x = x;
-    let mut cur_y = y;
-
-    // Skip checking for first character since we already know its correct.
-    for char in &search_arr[1..] {
-        cur_x += i;
-        cur_y += j;
-
-        if cur_x < 0 {
-            return false;
-        }
-        if cur_y < 0 {
-            return false;
-        }
-        if cur_y >= arr.len() as isize {
-            return false
-        }
-        if cur_x >= arr[cur_y as usize].len() as isize {
-            return false;
-        }
-        if arr[cur_y as usize][cur_x as usize] != *char {
-            return false;
-        }
+    if matches == 2 {
+        return 1
     }
-    return true;
+    return 0
 }
