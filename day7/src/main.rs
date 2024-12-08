5327fedaa60a8eb0da78893aeaa1bd70a74e98e6
FranseFrikandel <crgamingserver@hotmail.com>
2024-12-08 02:42:33 +0100

    Day 7 part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -5,11 +5,19 @@ fn main() {
     let timer = Instant::now();
     let mut corr_sum = 0;
     for line in read_to_string("input.txt").unwrap().lines() {
+        println!("{}", line);
         let line_arr: Vec<&str> = line.split(": ").collect();
         let res: usize = line_arr[0].parse().expect("Cant parse to int");
         let parameters: Vec<usize> = line_arr[1].split(" ").map(|str| str.parse().expect("Cant parse to int")).collect();
-        for i in 0..2_usize.pow((parameters.len()-1) as u32) {
-            if calculate(&parameters, i) == res {
+        for operators in 0..4_usize.pow((parameters.len()-1) as u32) {
+            for j in 0..std::mem::size_of::<usize>()/2 {
+                let operator_pair = operators & (0b11 << j*2);
+                if operator_pair & operator_pair >> 1 != 0 {
+                    // There's a case where both operators are set, skip this.
+                    continue
+                }
+            }
+            if calculate(&parameters, operators) == res {
                 corr_sum += res;
                 break;
             }
@@ -21,11 +29,15 @@ fn main() {
 
 fn calculate(params: &Vec<usize>, operators: usize) -> usize {
     let mut sum = params[0];
-    for i in 1..params.len() {
-        if operators & (1 << i-1) != 0 {
-            sum = sum + params[i];
+    for i in 0..params.len()-1 {
+        if operators & (0b01 << i*2) != 0 {
+            let sum_str = sum.to_string();
+            let param_str = params[i+1].to_string();
+            sum = (sum_str + &param_str).parse().expect("Concat failed");
+        } else if operators & (0b10 << i*2) != 0 {
+            sum = sum * params[i+1];
         } else {
-            sum = sum * params[i];
+            sum = sum + params[i+1];
         }
     }
     return sum;
--- readme.md
+++ readme.md
@@ -9,3 +9,16 @@ My attempt at learning Rust by doing the advent of code 2024.
 In day 6, the initial full solution had a runtime of 18.73s on my machine.
 After only checking for adding obstructions in places where the guard travelled in the first run, runtime was reduced to
 ~4 seconds.
+
+## Day 7
+
+Part 1 could be done using a single bit to define the operator.
+Part 2 requires 2 bits, since there's 3 operator.
+The operands would look something like:
+00 -> add
+01 -> concat
+10 -> multiply
+
+However, we need to make the parameter number not contain 11, since there's no 4th operator.
+
+Got it working this way, only the runtime is very long (>10 min on my laptop).
