855942e09e351da103d4642b78bb97ef057618ce
FranseFrikandel <romain@ceracarbon-racing.com>
2024-12-09 18:35:47 +0100

    Day 8 part 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -27,27 +27,18 @@ fn main() {
         }
     }
 
-    let antinode_w: Vec<bool> = vec![false; x_size+1];
-    let mut antinodes: Vec<Vec<bool>> = vec![antinode_w; y_size+1];
+    x_size += 1;
+    y_size += 1;
+
+    let antinode_w: Vec<bool> = vec![false; x_size];
+    let mut antinodes: Vec<Vec<bool>> = vec![antinode_w; y_size];
     for (_freq, freq_antennas) in &mut antennas {
         while freq_antennas.len() >= 2 {
             let last_antenna = freq_antennas.pop().expect("Could not pop antenna");
             for antenna in &freq_antennas[0..freq_antennas.len()] {
-                let anti_a;
-                let anti_b;
-                (anti_a, anti_b) = calculate_antinodes(antenna, &last_antenna);
-                for anti in [anti_a, anti_b] {
-                    if !anti.contains(&None) {
-                        match antinodes.get_mut(anti[0].expect("")) {
-                            Some(line) => {
-                                match line.get_mut(anti[1].expect("")) {
-                                    Some(node) => *node = true,
-                                    None => {}
-                                }
-                            },
-                            None => {}
-                        }
-                    }
+                let antinode_pair = calculate_antinodes(antenna, &last_antenna, y_size, x_size);
+                for anti in antinode_pair {
+                    antinodes[anti[0]][anti[1]] = true;
                 }
             }
         }
@@ -65,8 +56,36 @@ fn main() {
     println!("Total runtime: {:.3?}", timer.elapsed());
 }
 
-fn calculate_antinodes(a: &[usize; 2], b: &[usize; 2]) -> ([Option<usize>; 2], [Option<usize>; 2]) {
-    let antinode_a = [(2*a[0]).checked_sub(b[0]), (2*a[1]).checked_sub(b[1])];
-    let antinode_b = [(2*b[0]).checked_sub(a[0]), (2*b[1]).checked_sub(a[1])];
-    return (antinode_a, antinode_b);
+fn calculate_antinodes(a: &[usize; 2], b: &[usize; 2], max_y: usize, max_x: usize) -> Vec<[usize; 2]> {
+    let mut antinodes: Vec<[usize; 2]> = Vec::new();
+    let diff: [isize; 2] = [b[0] as isize - a[0] as isize, b[1] as isize - a[1] as isize];
+
+    antinodes.push(*a);
+    let mut cur_pos = [a[0] as isize, a[1] as isize];
+    // Loop through all a+diff options
+    loop {
+        cur_pos[0] = cur_pos[0] + diff[0];
+        cur_pos[1] = cur_pos[1] + diff[1];
+        if cur_pos[0] < 0 || cur_pos[1] < 0 {
+            break;
+        }
+        if cur_pos[0] as usize >= max_y || cur_pos[1] as usize >= max_x {
+            break;
+        }
+        antinodes.push([cur_pos[0] as usize, cur_pos[1] as usize]);
+    };
+    
+    let mut cur_pos = [a[0] as isize, a[1] as isize];
+    loop {
+        cur_pos[0] = cur_pos[0] - diff[0];
+        cur_pos[1] = cur_pos[1] - diff[1];
+        if cur_pos[0] < 0 || cur_pos[1] < 0 {
+            break;
+        }
+        if cur_pos[0] as usize >= max_y || cur_pos[1] as usize >= max_x {
+            break;
+        }
+        antinodes.push([cur_pos[0] as usize, cur_pos[1] as usize]);
+    };
+    return antinodes;
 }
