5d1012455f52bcdcacc66421f4b5f7c41011b5e5
FranseFrikandel <romain@ceracarbon-racing.com>
2024-12-09 21:13:55 +0100

    More optimized day 7

---
+++ day7_opt/src/main.rs
@@ -0,0 +1,47 @@
+use std::fs::read_to_string;
+use std::time::Instant;
+
+fn main() {
+    let timer = Instant::now();
+    let mut corr_sum = 0;
+    for line in read_to_string("input.txt").unwrap().lines() {
+        let line_arr: Vec<&str> = line.split(": ").collect();
+        let res: usize = line_arr[0].parse().expect("Cant parse to int");
+        let parameters: Vec<usize> = line_arr[1].split(" ").map(|str| str.parse().expect("Cant parse to int")).collect();
+        let poss_res = calculate(&parameters, res);
+        if poss_res.contains(&res) {
+            corr_sum += res;
+        }
+    }
+    // assert_eq!(corr_sum, 426214131924213);
+    println!("Total amount of correct formula's: {}", corr_sum);
+    println!("Total runtime: {:.3?}", timer.elapsed());
+}
+
+fn calculate(params: &[usize], res: usize) -> Vec<usize> {
+    let mut possible_res = Vec::new();
+    if params.len() <= 2 {
+        possible_res.push(params[0] + params[1]);
+        possible_res.push(params[0] * params[1]);
+        let concat = params[0].to_string() + &params[1].to_string();
+        possible_res.push(concat.parse().expect("Failed to concat"));
+    } else {
+        let param = params.last().expect("Failed to retrieve last item");
+        let results = calculate(&params[0..params.len()-1], res);
+        for res in results {
+            let sum = param + res;
+            // if sum <= res {
+                possible_res.push(sum);
+            // }
+            let mult = param * res;
+            // if mult <= res {
+                possible_res.push(mult);
+            // }
+            let concat = (res.to_string() + &param.to_string()).parse().expect("Failed to concat");
+            // if concat <= res {
+                possible_res.push(concat);
+            // }
+        }
+    }
+    return possible_res;
+}
