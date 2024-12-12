e29ff0c5ecc963b926b834766457c6e25ea3cfd9
FranseFrikandel <romain@ceracarbon-racing.com>
2024-12-12 18:42:48 +0100

    Day 12 p2 (finally)

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -25,42 +25,110 @@ fn main() {
     }
 
     // Calculate the border and area for each region
-    let mut sum = 0;
+    let mut p1_sum = 0;
+    let mut p2_sum = 0;
     for region in regions {
         let area = region.1.len();
         let mut border = 0;
+        // Border pieces sorted by coordinate and side
+        let mut border_pieces: Vec<[usize; 3]> = Vec::new();
         for point in &region.1 {
             match get_2d(point[0], point[1]+1, &original_map) {
-                Some(plant) => if plant != region.0 {border += 1}
-                None => {border += 1}
+                Some(plant) => if plant != region.0 {
+                    border += 1;
+                    border_pieces.push([point[0], point[1], 0]);
+                }
+                None => {
+                    border += 1;
+                    border_pieces.push([point[0], point[1], 0]);
+                }
             }
             match get_2d(point[0]+1, point[1], &original_map) {
-                Some(plant) => if plant != region.0 {border += 1}
-                None => {border += 1}
+                Some(plant) => if plant != region.0 {
+                    border += 1;
+                    border_pieces.push([point[0], point[1], 1]);
+                }
+                None => {
+                    border += 1;
+                    border_pieces.push([point[0], point[1], 1]);
+                }
             }
             if point[1] > 0 {
                 match get_2d(point[0], point[1]-1, &original_map) {
-                    Some(plant) => if plant != region.0 {border += 1}
-                    None => {border += 1}
+                    Some(plant) => if plant != region.0 {
+                        border += 1;
+                        border_pieces.push([point[0], point[1], 2]);
+                    }
+                    None => {
+                        border += 1;
+                        border_pieces.push([point[0], point[1], 2]);
+                    }
                 }
             } else {
                 border += 1;
+                border_pieces.push([point[0], point[1], 2]);
             }
             if point[0] > 0 {
                 match get_2d(point[0]-1, point[1], &original_map) {
-                    Some(plant) => if plant != region.0 {border += 1}
-                    None => {border += 1}
+                    Some(plant) => if plant != region.0 {
+                        border += 1;
+                        border_pieces.push([point[0], point[1], 3]);
+                    }
+                    None => {
+                        border += 1;
+                        border_pieces.push([point[0], point[1], 3]);
+                    }
                 }
             } else {
                 border += 1;
+                border_pieces.push([point[0], point[1], 3]);
             }
         }
-        // println!("{}: {:?}", region.0, region.1);
-        // println!("{}: {} * {}", region.0, area, border);
-        sum += area*border;
+
+        let border_pieces_cpy = border_pieces.clone();
+        for piece in border_pieces_cpy {
+            // If we've already removed this piece from the non-copied array, we've already added this piece as a perimeter.
+            if !border_pieces.contains(&piece) {continue}
+            if piece[2] == 1 || piece[2] == 3 {
+                // Oriented along the X-axis (2nd index)
+                let mut i = 1;
+                loop {
+                    let Some(pos) = border_pieces.iter().position(|&p| p == [piece[0], piece[1]+i, piece[2]]) else {break};
+                    border_pieces.remove(pos);
+                    i += 1;
+                }
+                i = 1;
+                loop {
+                    if i > piece[1] {break}
+                    let Some(pos) = border_pieces.iter().position(|&p| p == [piece[0], piece[1]-i, piece[2]]) else {break};
+                    border_pieces.remove(pos);
+                    i += 1;
+                }
+            } else {
+                // Oriented along the Y-axis (1st index)
+                let mut i = 1;
+                loop{
+                    let Some(pos) = border_pieces.iter().position(|&p| p == [piece[0]+i, piece[1], piece[2]]) else {break};
+                    border_pieces.remove(pos);
+                    i += 1;
+                }
+                i = 1;
+                loop {
+                    if i > piece[0] {break}
+                    let Some(pos) = border_pieces.iter().position(|&p| p == [piece[0]-i, piece[1], piece[2]]) else {break};
+                    border_pieces.remove(pos);
+                    i += 1;
+                }
+            }
+        }
+        println!("{:?}", border_pieces);
+        println!("{}: {} * {}", region.0, area, border_pieces.len());
+        p1_sum += area*border;
+        p2_sum += area*border_pieces.len();
     }
 
-    println!("Total cost: {}", sum);
+    println!("Total part 1 cost: {}", p1_sum);
+    println!("Total part 2 cost: {}", p2_sum);
     println!("Total runtime: {:.3?}", timer.elapsed());
 }
 
