3d95d0fa067a5c403b3157b2f65bc58bff57155e
FranseFrikandel <romain@ceracarbon-racing.com>
2024-12-20 18:16:07 +0100

    Day 19 p2
    I thought memoization wasen't going to change the performance to go from unusable to usable. Turns out it did...

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,26 +1,34 @@
+use std::collections::{HashMap, HashSet};
 use std::time::Instant;
-use std::thread;
 use std::fs::read_to_string;
+use std::cmp;
 // use itertools::Itertools;
+const NUM_HASHSETS: usize = 10;
 
 fn main() {
     let timer = Instant::now();
 
     let filestr = read_to_string("input.txt").unwrap();
     let mut lines = filestr.lines();
-    let mut towels: Vec<&str> = lines.next().unwrap().split(", ").collect();
-    // Sorting from large to small seems to speed up the example input, but not the actual input
-    // towels.sort_by(|a, b| b.len().cmp(&a.len()));
+    // Split into seperate hashset for each length. Need to make sure there's enough hashmaps for all lengths.
+    // Edit: This did not help at all with performance.
+    let mut towels: [HashSet<&str>; NUM_HASHSETS] = Default::default();
+    let mut max_towel_length = 0;
+
+    for towel in lines.next().unwrap().split(", ") {
+        towels[towel.len()-1].insert(towel);
+        max_towel_length = cmp::max(max_towel_length, towel.len());
+    }
+
     let mut p1_sum = 0;
     let mut p2_sum = 0;
-    // for line_chunk in lines.chunks(50) {
 
-    // }
     for line in lines {
         if line.len() == 0 {
             continue;
         }
-        let sum = try_finish_line(line, &towels);
+        let mut cache: HashMap<&str, usize> = HashMap::new();
+        let sum = try_finish_line(line, &towels, max_towel_length, &mut cache);
         p2_sum += sum;
         if sum > 0 {
             p1_sum += 1;
@@ -32,19 +40,23 @@ fn main() {
     println!("Total runtime: {:.3?}", timer.elapsed());
 }
 
-fn try_finish_line(line: &str, towels: &Vec<&str>) -> usize {
+fn try_finish_line<'a>(
+    line: &'a str, 
+    towels: &[HashSet<&str>; NUM_HASHSETS], 
+    max_len: usize, 
+    cache: &mut HashMap<&'a str, usize>) -> usize 
+{
     let mut sum = 0;
-    if line.len() == 0 {
-        return 1;
+    if line.len() == 0 {return 1}
+    if let Some(v) = cache.get(line) {return *v}
+    for i in 0..cmp::min(max_len, line.len()) {
+        let lineseg = &line[0..i+1];
+        let towel_group = &towels[i];
+        if towel_group.contains(lineseg) {
+            let stripped_line = &line[i+1..];
+            sum += try_finish_line(stripped_line, towels, max_len, cache);
+        }
     }
-    for towel in towels.iter() {
-        if line.starts_with(towel) {
-            let stripped_line = &line[towel.len()..];
-            sum += try_finish_line(stripped_line, towels);
-            if sum == 1 {
-                break
-            }
-        };
-    }
+    cache.insert(line, sum);
     return sum;
 }
